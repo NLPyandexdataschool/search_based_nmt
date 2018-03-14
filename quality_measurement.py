b8c462f0fc5667597d4d6b722b080c6ee7e62652
Pavel Koshkin <koshkin.pasha@gmail.com>
2018-03-14 17:34:35 +0300

    Cosmetic change

--- quality_measurement.py
+++ quality_measurement.py
@@ -4,16 +4,15 @@ from nltk.translate.bleu_score import sentence_bleu
 
 
 def measure_quality(predicted_file_name, answers_file_name):
-    with open(predicted_file_name) as predict_handler:
-        with open(answers_file_name) as answers_handler:
-            predictions = [line[:-1].split() for line in predict_handler]
-            answers = [line[:-1] for line in answers_handler]
-            if len(predictions) != len(answers):
-                raise Exception('Input files must have same length!')
-            score = np.mean([
-                sentence_bleu(prediction, answer)
-                for answer, prediction in zip(answers, predictions)
-            ])
+    with open(predicted_file_name) as predict_handler, open(answers_file_name) as answers_handler:
+        predictions = [line[:-1].split() for line in predict_handler]
+        answers = [line[:-1] for line in answers_handler]
+        if len(predictions) != len(answers):
+            raise Exception('Input files must have same length!')
+        score = np.mean([
+            sentence_bleu(prediction, answer)
+            for answer, prediction in zip(answers, predictions)
+        ])
     return score
 
 
