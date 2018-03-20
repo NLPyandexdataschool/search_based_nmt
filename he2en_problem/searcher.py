ab4e52efce58dc92b27ee4de0354308d0a1b817f
Pavel Koshkin <koshkin.pasha@gmail.com>
2018-03-20 23:53:36 +0300

    searcher work in progress

---
+++ he2en_problem/searcher.py
@@ -0,0 +1,43 @@
+from nltk.metrics.distance import edit_distance
+from heapq import heappush, heappushpop, heappop
+
+
+class SearcherException(Exception):
+    pass
+
+
+class Searcher:
+    def __init__(self, file_names):
+        '''
+        Argument file_names is a list of files to search names.
+        '''
+        self.file_names = file_names
+
+    def distance(self, first_word, second_word):
+        return edit_distance(first_word, second_word)
+
+    def process_table(self, table_file_name):
+        data = []
+        for file_name in self.file_names:
+            with open(file_name) as handler:
+                data.extend([line.strip() for line in handler])
+        return data
+
+    def search(self, word, n_nearest=10):
+        '''
+        Search n nearest words to given word.
+        '''
+        heap = []
+        for file_name in self.file_names:
+            with open(file_name) as handler:
+                for line in handler:
+                    item = (-self.distance(word.strip(), line.strip()), line.strip())
+                    if item in heap:
+                        continue
+                    if len(heap) < n_nearest:
+                        heappush(heap, item)
+                    else:
+                        heappushpop(heap, item)
+        if len(heap) != n_nearest:
+            raise SearcherException("Wrong result length!")
+        return list(reversed([heappop(heap)[1] for _ in range(n_nearest)]))
