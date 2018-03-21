a957449bc5cede72a0fc04b51759d39d0f5ecc28
Boialex <xdr007@gmail.com>
2018-03-22 00:34:10 +0300

    data split and run on parts

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -1,4 +1,11 @@
-./reset_model.sh $@
-./run_datagen.sh $@
-./run_train.sh $@
-./run_eval.sh $@
+#!/usr/bin/env bash
+SCRIPTS_PATH=$(dirname $0)
+
+echo run_datagen &&
+$SCRIPTS_PATH/run_datagen.sh $@ &&
+echo &&
+echo run_train &&
+$SCRIPTS_PATH/run_train.sh $@ &&
+echo &&
+echo run_eval &&
+$SCRIPTS_PATH/run_eval.sh $@
