71f2606c97aa06d8febb5a6c421febcc2ef2a60e
Pavel Koshkin <koshkin.pasha@gmail.com>
2018-03-25 13:31:59 +0300

    Added file to search to translator

--- search_based_nmt/search_engine/translator.py
+++ search_based_nmt/search_engine/translator.py
@@ -2,17 +2,20 @@ from collections import defaultdict
 
 
 class Translator:
-    def __init__(self, path):
+    def __init__(self, path_to_raw_data, file_to_search_name):
         endings = ['.test.txt', '.train.txt', '.dev.txt']
         data = []
         for ending in endings:
-            with open(path + '/en' + ending) as en_file:
-                with open(path + '/he' + ending) as he_file:
+            with open(path_to_raw_data + '/en' + ending) as en_file:
+                with open(path_to_raw_data + '/he' + ending) as he_file:
                     data.extend(zip(he_file, en_file))
         data = list(set(data))
         self.dict = defaultdict(list)
+        with open(file_to_search_name) as handler:
+            words_to_search = set([line.strip() for line in handler])
         for he_word, en_word in data:
-            self.dict[he_word.strip()].append(en_word.strip())
+            if he_word in words_to_search:
+                self.dict[he_word.strip()].append(en_word.strip())
 
     def translate(self, he_word):
         return self.dict[he_word]
