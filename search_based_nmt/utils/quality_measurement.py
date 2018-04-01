26c4ec5bd78276b772b65f729e5eb3038da916e8
Pavel Koshkin <koshkin.pasha@gmail.com>
2018-04-01 11:35:31 +0300

    Cosmetic fixes.

--- search_based_nmt/utils/quality_measurement.py
+++ search_based_nmt/utils/quality_measurement.py
@@ -7,30 +7,30 @@ from collections import defaultdict
 
 
 def measure_quality(references_file_name, sources_file_name, hypotheses_file_name):
-    with open(references_file_name) as references_handler:
-        with open(sources_file_name) as sources_handler:
-            with open(hypotheses_file_name) as hypotheses_handler:
+    with open(references_file_name) as references_handler,\
+         open(sources_file_name) as sources_handler,\
+         open(hypotheses_file_name) as hypotheses_handler:
                 references_dict = defaultdict(list)
                 hypotheses_dict = {}
                 for reference, source, hypothesis in zip(references_handler, sources_handler, hypotheses_handler):
                     references_dict[source.strip()].append(reference.strip())
                     hypotheses_dict[source.strip()] = hypothesis.strip()
-        answers = [hypotheses_dict[key] for key in hypotheses_dict]
-        predictions = [references_dict[key] for key in hypotheses_dict]
-        # no smoothing
-        smoothie = SmoothingFunction().method0
-        score = np.mean([
-            sentence_bleu(prediction, answer, smoothing_function=smoothie)
-            for answer, prediction in zip(answers, predictions)
-        ])
-    return score
+                predictions = [hypotheses_dict[key] for key in hypotheses_dict]
+                targets = [references_dict[key] for key in hypotheses_dict]
+                # no smoothing
+                smoothie = SmoothingFunction().method0
+                score = np.mean([
+                    sentence_bleu(target, prediction, smoothing_function=smoothie)
+                    for target, prediction in zip(targets, predictions)
+                ])
+                return score
 
 
 if __name__ == '__main__':
     warnings.filterwarnings("ignore")
     parser = argparse.ArgumentParser()
-    parser.add_argument(type=str, dest='references')
-    parser.add_argument(type=str, dest='sources')
-    parser.add_argument(type=str, dest='hypotheses')
+    parser.add_argument(type=str, dest='references', help='File with references.')
+    parser.add_argument(type=str, dest='sources', help='File with sources.')
+    parser.add_argument(type=str, dest='hypotheses', help='File with hypotheses.')
     args = parser.parse_args()
     print(measure_quality(args.references, args.sources, args.hypotheses))
