825282c66d73f8386366cf64029c7abcee2b8ed5
Pavel Koshkin <koshkin.pasha@gmail.com>
2018-04-03 12:11:24 +0300

    Preprocessing direct search table.

--- search_based_nmt/search_engine/translator.py
+++ search_based_nmt/search_engine/translator.py
@@ -1,4 +1,6 @@
 from collections import defaultdict
+from os.path import join
+from numpy.random import choice
 
 
 class Translator:
@@ -6,16 +8,19 @@ class Translator:
         endings = ['.test.txt', '.train.txt', '.dev.txt']
         data = []
         for ending in endings:
-            with open(path_to_raw_data + '/en' + ending) as en_file:
-                with open(path_to_raw_data + '/he' + ending) as he_file:
+            with open(join(path_to_raw_data, 'en' + ending)) as en_file:
+                with open(join(path_to_raw_data, 'he' + ending)) as he_file:
                     data.extend(zip(he_file, en_file))
-        data = list(set(data))
+        data = set(data)
         self.dict = defaultdict(list)
         with open(file_to_search_name) as handler:
             words_to_search = set([line.strip() for line in handler])
         for he_word, en_word in data:
-            if he_word in words_to_search:
+            if he_word.strip() in words_to_search:
                 self.dict[he_word.strip()].append(en_word.strip())
 
-    def translate(self, he_word):
+    def translate_all(self, he_word):
         return self.dict[he_word]
+
+    def translate_random(self, he_word):
+        return choice(self.dict[he_word])
